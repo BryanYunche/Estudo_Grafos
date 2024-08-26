0de8a02a5b61661b5d6dcc6d201592d511ad70c4
BryanYunche <estudanteporesporte@gmail.com>
2024-08-26 00:05:40 -0300

    Trabalho de Grafos 01, representação computacional de grafo

---
+++ Exercicios_Praticos/Atividade01/main.py
@@ -0,0 +1,10 @@
+from Grafo_Matriz_Lista import Grafo
+
+grafo01 = Grafo()
+
+#Cadastra o Grafo
+grafo01.cadastrarGrafos()
+
+#Mostra as informações do Grafo
+print(f'Vestices do Grafo: {grafo01.getVertices()}')
+print(f'Arestas do Grafo: {grafo01.getArestas()}')
