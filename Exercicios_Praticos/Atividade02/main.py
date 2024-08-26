3886ca22c301c6acb4e54647d2017bab7798dfd5
unknown <estudanteporesporte@gmail.com>
2024-08-26 11:51:00 -0300

    Feita a lógica do programa de Produto cartesiano

--- Exercicios_Praticos/Atividade02/main.py
+++ Exercicios_Praticos/Atividade02/main.py
@@ -26,3 +26,19 @@ arestas2 = grafo02.getArestas()
 
 #Formato que está a lista de vertices: ['a', 'b', 'c', 'd']
 #Formato que está a lista de arestas: [(a, b), (b, c), (c, a), (d, b)]
+
+#Formato que está a lista de vertices: ['1', '2', '3', '4']
+#Formato que está a lista de arestas: [('1', '2'), ('2', '3'), ('3', '1'), ('4', '2')]
+
+#Lógica
+#Multiplicar cada aresta do Grafo 01 por cada vertice do Grafo 02
+    # Selecionar a aresta do Grafo 01 (x, y)
+    # Multiplicar cada vertice do Grafo 02 , encontrado nas arestas, pela aresta (x, y) do Grafo 01
+    # Sendo assim, cada vertice vai ocupar o a possição y no novo par ordenado gerado 
+#Multiplicar cada aresta do Grafo 02 por cada vertice do Grafo 01
+    # Selecionar a aresta do Grafo 02 (x, y)
+    # Multiplicar cada vertice do Grafo 01 , encontrado nas arestas, pela aresta (x, y) do Grafo 02
+    # Sendo assim, cada vertice vai ocupar o a possição x no novo par ordenado gerado
+#Multiplicar cada vertice por cada vertice
+    #Simplexmente vai ser V1 X V2
+
