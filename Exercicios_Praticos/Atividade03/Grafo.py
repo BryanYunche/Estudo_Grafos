c212dc96d428100fa1e891b756f24be11cfb5d75
BryanYunche <estudanteporesporte@gmail.com>
2024-09-19 00:55:37 -0300

    Refatorização de grafo

--- Exercicios_Praticos/Atividade03/Grafo.py
+++ Exercicios_Praticos/Atividade03/Grafo.py
@@ -6,24 +6,48 @@ class Grafo():
         self.matrizAdj = matrizAdj
         self.listaAdj = listaAdj
 
-    def getArestas(self):
+    def get_arestas(self):
         return self.arestas
     
-    def setArestas(self, aresta):
+    def set_arestas(self, aresta):
         self.arestas += aresta
     
-    def getVertices(self):
+    def get_vertices(self):
         return self.arestas
     
-    def setVertices(self, vertices):
+    def set_vertices(self, vertices):
         self.arestas += vertices
+
+    def get_matriz_adj(self, matrizAdj):
+        return self.matrizAdj
     
-    def matrizAdj(self, matriz):
-        #Criar de maneira autômática identificadores pra linha e coluna de cada grafo
+    def set_matriz_adj(self, matrizAdj):
+        self.matrizAdj = matrizAdj
+
+    def gera_matriz_adj(self, matriz):
+        #Verifica antes se existe vertices
+        #Gera matriz de adjacencias baseado nos vertices e nas arestas
         pass
     
-    def listaAdj(self, listaAdj):
-        #
+    def imprime_matriz_adj(self, matriz):
+        #Mostra um print da matriz
         pass
+
+    def gera_lista_adj(self, listaAdj):
+        #verifica se existe vertices
+        #Gera lista de adjácencias baseado nos vertices e arestas
+        pass
+    
+    #Função interna do grafo
+    def consome_matriz(self, matriz):
+        #Consome a matriz de rastreabilidade do grafo para gerar vertices e arestas
+        #A matriz só tem 0 e 1
+        pass
+
+    #Função interna do grafo
+    def consome_lista(self, lista):
+        #Consome a lista de adjácencias e gera vertices e arestas
+        pass
+
     
         
