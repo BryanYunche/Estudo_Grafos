07ef1b1d6662952fde5fa75901e9c8f545fbc68c
BryanYunche <estudanteporesporte@gmail.com>
2024-09-22 16:31:11 -0300

    Trabalho do DFS feito

---
+++ Exercicios_Praticos/03_Representa_DFS/DFS.py
@@ -0,0 +1,85 @@
+from Grafo import Grafo
+
+#Classe DFS herda da classe Grafo
+class DFS(Grafo):
+    
+    def __init__(self):
+        #Chama o construtor de grafo
+        super().__init__()
+        
+        #Adiciona novos atributos
+        self.arvores = 0
+        self.ciclos = 0
+        self.tempo = 0
+        self.verticeAnterior = None
+        self.dicionarioGrafos = []
+
+
+    def get_dicionario_grafos(self):
+        return self.dicionarioGrafos
+
+    #Constroi dicionário de grafos Brancos
+    def DFS_constroi_dicionario(self):
+        #Gera a lista de aDjacências caso não exista ainda 
+        self.gera_lista_adj()
+
+        #Constroi os dicionários
+        for vertice in self.get_lista_adj():
+            #O if não altera a ordem dos vertices
+            #As adjácencias guardam o indice de onde está o vertice no dicionário
+            if len(vertice) >= 1:
+                #Considerando que a ordem das listas de adjacencias é igual a ordem da lista de vertices
+                #Usarei a lista de vertices para identificar o indice dos dicionários
+                #Substituirei as adjacencias pelo indice dos vertices que seriam essas adjacências
+                self.dicionarioGrafos.append({'Vertice': vertice[0], 
+                                              'Adjacencia': [self.get_vertices().index(verticeAdj) for verticeAdj in vertice[1:] if verticeAdj in self.get_vertices()], 
+                                              'Cor': 'Branco', 
+                                              'Anterior': None,
+                                              'Inicio': 0, 
+                                              'Fim': 0})  
+            elif len(vertice) == 1:
+                self.dicionarioGrafos.append({'Vertice':vertice[0], 
+                                              'Adjacencia': None, 
+                                              'Cor': 'Branco', 
+                                              'Anterior': None,
+                                              'Inicio': 0, 
+                                              'Fim': 0})
+            
+    def DFS_procura(self):
+        for i in range(len(self.dicionarioGrafos)):
+            if self.dicionarioGrafos[i].get('Cor') == 'Branco':
+                self.dicionarioGrafos[i]['Anterior'] = 'Raiz'
+                self.DFS_visita_vertice(self.dicionarioGrafos[i].get('Vertice'), i)
+                
+                #A cada vez que o algoritimo volta a esse laço, quer dizer que ele saiu de uma arvore
+                #Logo, pode ser identificado uma arvore
+                self.arvores += 1 
+
+    #Percorre Grafo (Terminar de fazer isso)
+    def DFS_visita_vertice(self, verticeAnterior, indiceVisitado):
+        self.dicionarioGrafos[indiceVisitado]['Cor'] = 'Cinza'
+        self.dicionarioGrafos[indiceVisitado]['Inicio'] = self.tempo
+
+        #Verifica se o vertice não é uma Raiz
+        if self.dicionarioGrafos[indiceVisitado]['Anterior'] != 'Raiz':
+           self.dicionarioGrafos[indiceVisitado]['Anterior'] = self.verticeAnterior
+
+        #Conta um ao tempo do contador
+        self.tempo += 1
+        for indiceVerticeAdj in self.dicionarioGrafos[indiceVisitado].get('Adjacencia'):
+            if self.dicionarioGrafos[indiceVerticeAdj]['Cor'] == 'Branco':
+                self.verticeAnterior = self.dicionarioGrafos[indiceVisitado].get('Vertice')
+                self.DFS_visita_vertice(verticeAnterior, indiceVerticeAdj)
+            
+            #Identifica ciclos no grafo
+            elif self.dicionarioGrafos[indiceVerticeAdj]['Cor'] == 'Cinza':
+                self.ciclos += 1
+        #Conta um ao finalizar
+        self.tempo += 1
+
+        #Pinta de Preto caso saia da pilha
+        self.dicionarioGrafos[indiceVisitado]['Cor'] = 'Preto'
+        self.dicionarioGrafos[indiceVisitado]['Fim'] = self.tempo
+        
+            
+
--- Exercicios_Praticos/03_Representa_DFS/main.py
+++ Exercicios_Praticos/03_Representa_DFS/main.py
@@ -1,13 +1,21 @@
-lista = [1, 2, 3, 4, 5]
-arestas = [(1,3), (2,5), (4,3), (2,2), (4,5)]
-numVertices = len(lista)
-matrizAdj = [[0 for _ in range(numVertices)] for _ in range(numVertices)]
+from DFS import DFS
 
-print(matrizAdj)
+#Cria um objeto do tipo Grafo
+grafo01 = DFS()
 
-for verticeY in lista:
-    for verticeX in lista:
-        for aresta in arestas:
-            x, y = aresta
-            if (verticeX == x and verticeY == y):
-                pass 
+#Adiciona vertices
+grafo01.set_vertices(['A', 'B','C', 'D', 'E','F', 'G', 'H','I'])
+grafo01.set_arestas([('A', 'B'), ('F', 'G'), ('H', 'F'), ('D', 'H'), ('C', 'D'), ('A', 'C'), ('F', 'I'), ('H', 'I')])
+
+grafo01.DFS_constroi_dicionario()
+for dicionario in range(len(grafo01.get_dicionario_grafos())):
+    print(grafo01.get_dicionario_grafos()[dicionario])
+
+print('=====================================================')
+grafo01.DFS_procura()
+for dicionario in range(len(grafo01.get_dicionario_grafos())):
+    print(grafo01.get_dicionario_grafos()[dicionario])
+
+print('=====================================================')
+print(f'Ciclos do grafo: {grafo01.ciclos}')
+print(f'Número de Arvores: {grafo01.arvores}')
